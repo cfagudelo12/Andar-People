a914ae6df05090700c6b422258fec698067ea6ee
cfagudelo12 <cf.agudelo12@uniandes.edu.co>
2017-02-18 15:03:33 -0500

    Carreras

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     %w(C.C. T.I.)
   end
   def self.careers_list
+    ["Administración", "Contaduría Internacional", "Arquitectura", "Diseño", "Arte", "Historia del Arte", "Literatura", "Música", "Biología", "Física", "Geociencias", "Matemáticas", "Microbiología", "Química", "Antropología", "Ciencia Política", "Filosofía", "Historia", "Lenguas y Cultura", "Psicología", "Derecho", "Economía", "Licenciatura en Artes", "Licenciatura en Ciencias Naturales", "Licenciatura en Ciencias Sociales", "Licenciatura en Educación para la Primera Infancia", "Licenciatura en Humanidades", "Licenciatura en Matemáticas", "Ingeniería Ambiental", "Ingeniería Biomédica", "Ingeniería Civil", "Ingeniería Eléctrica", "Ingeniería Electrónica", "Ingeniería Industrial", "Ingeniería Mecánica", "Ingeniería Química", "Ingeniería de Sistemas y Computación", "Medicina", "Gobierno y Asuntos Públicos"]
   end
   has_many :observations
   mount_uploader :photo, PhotoUploader
